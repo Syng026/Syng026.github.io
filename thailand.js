18ff08061d1418af2f46684e435fd93b9c815462
Syng026 <goldfishlady03@gmail.com>
2024-07-16 13:59:12 -0400

    Update thailand.js

--- thailand.js
+++ thailand.js
@@ -1,1 +1,10 @@
+// 15.8700° N, 100.9925° E -> 15.8700, 100.9925
+let map = L.map("map", {
+  center: [15.8700, 100.9925], 
+  zoom: 15
+});
 
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
